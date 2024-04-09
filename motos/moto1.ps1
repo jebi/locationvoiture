143c93241fdaf3ea2eeef4dc1bb03c6efee7d93f
Jean-Philippe Biasin <Biasin@france-education-international.fr>
2024-04-09 13:24:23 +0200

    ajout des motos

---
+++ motos/moto1.ps1
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Moto1</title>
+    </head>
+    <body style="background-color:#bdc3a7">
+	<h1 style="padding-top:20%; padding-botton:20%; text-align:center;color:#FFFFFF">Moto 1</h1>
+    </body>
+</html>
